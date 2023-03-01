d8ea8bb8e34634f92beb8ec46ac4c65126137990
Jhon Oderman <jhooderman@gmail.com>
2023-03-01 03:16:54 -0500

    fixed untracked files

--- config/conexion-datos.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servidorConexion = 'sintia.co';
-$usuarioConexion = 'mobiliar';
-$claveConexion = 'M1X32znd9l';
-$baseDatosServicios = 'mobiliar_sintia_admin'; 
-$baseDatosMarketPlace = 'mobiliar_sintia_marketplace';
-$baseDatosAdmisiones = 'mobiliar_sintia_admisiones';
