985bfe5fee9195bfc2ceb7cde5755e011e3889f2
Enuar Lara <enuar2110@gmail.com>
2023-07-11 09:11:05 -0500

    PES2023-275 - Mejora para envio de formulario de prueba gratis

--- prueba-gratis-guardar.php
+++ prueba-gratis-guardar.php
@@ -11,12 +11,10 @@ require 'library/phpmailer/SMTP.php';
 $bdDemo="mobiliar_dev_".date("Y");
 
 $clave = rand(10000, 99999);
-mysqli_query($conexion, "INSERT INTO $bdDemo.usuarios(uss_usuario, uss_clave, uss_tipo, uss_nombre, uss_idioma, uss_email, uss_bloqueado, uss_fecha_registro, uss_solicitar_datos, uss_celular)VALUES('" . $_POST["email"] . "', '" . $clave . "', 5, '" . $_POST["nombre"] . "', 1, '" . $_POST["email"] . "', 0, now(), 1, '" . $_POST["celular"] . "')");
+mysqli_query($conexion, "INSERT INTO $bdDemo.usuarios(uss_usuario, uss_clave, uss_tipo, uss_nombre, uss_idioma, uss_email, uss_bloqueado, uss_fecha_registro, uss_solicitar_datos, uss_celular)VALUES('" . $_POST["email"] . "', SHA1('" . $clave . "'), 5, '" . $_POST["nombre"] . "', 1, '" . $_POST["email"] . "', 0, now(), 1, '" . $_POST["celular"] . "')");
 $idRegistro = mysqli_insert_id($conexion);
-echo "- Se creó el usuario en la base de datos<br>";
 
 mysqli_query($conexion, "INSERT INTO demo(demo_fecha_ingreso, demo_usuario, demo_ip, demo_cantidad, demo_correo_enviado, demo_fecha_ultimo_correo, demo_nocorreos)VALUES(now(), '" . $idRegistro . "', '" . $_SERVER["REMOTE_ADDR"] . "', 0, 1, now(), 0)");
-echo "- Se creó el usuario en la tabla demo<br>";
 
 //INICIO ENVÍO DE MENSAJE
 $tituloMsj = "¡Bienvenido a la Plataforma SINTIA!";
@@ -31,7 +29,7 @@ $contenidoMsj = '
 			
 	<p>
 		<h3 style="text-align: center;">
-		<a href="https://developer.plataformasintia.com/app-sintia/main-app/guardar.php?get=1&usr=' . $idRegistro . '&user=' . $_POST["email"] . '&pass=' . $clave . '" target="_blank" style="color: #41c4c4; font-weight:bold;">ACCEDER A MI CUENTA DE DIRECTIVO AHORA</a>
+		<a href="https://developer.plataformasintia.com/app-sintia/main-app/" target="_blank" style="color: #41c4c4; font-weight:bold;">ACCEDER A MI CUENTA DE DIRECTIVO AHORA</a>
 		</h3>
 	</p>
 			
@@ -44,10 +42,8 @@ echo '<div style="display:none;">';
 try {
 	include("config/mail.php");
 
-	//$mail->addAddress($_POST["email"], $_POST["nombre"]);     // Add a recipient
-    $mail->addBCC($_POST["email"]);     // Add a recipient
-	$mail->addBCC('company@plataformasintia.com');     // Add a recipient
-	$mail->addBCC('corozco@unac.edu.co');     // Add a recipient
+    $mail->addAddress($_POST["email"], $_POST["nombre"]);     // Add a recipient
+	$mail->addAddress('soporte@plataformasintia.com', 'Soporte Plataforma SINTIA');
 
 	// Content
 	$mail->isHTML(true);                                  // Set email format to HTML
@@ -56,7 +52,6 @@ try {
 	$mail->CharSet = 'UTF-8';
 
     $mail->send();
-	echo 'Estamos creando su cuenta DEMO... Por favor espere un momento.';
 } catch (Exception $e) {
 	echo "Error: {$mail->ErrorInfo}";
 	exit();
