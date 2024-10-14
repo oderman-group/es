7bcd5363de462d4d6af7556063d167b95846fcf6
Jhon Oderman Mejia <jhooderman@gmail.com>
2024-10-14 11:21:42 -0500

    Mejora en envio de correos

--- solicitar-demo.php
+++ solicitar-demo.php
@@ -11,12 +11,25 @@ require 'library/phpmailer/SMTP.php';
 mysqli_query($conexion, "INSERT INTO demo(demo_ip, demo_nombre, demo_cargo, demo_telefono, demo_email, demo_institucion, demo_apellido, demo_comentario, demo_referencia)VALUES('" . $_SERVER["REMOTE_ADDR"] . "', '".$_POST['nombre']."', '".$_POST['cargo']."', '".$_POST['telefono']."', '".$_POST['email']."', '".$_POST['institucion']."', '".$_POST['apellido']."', '".$_POST['comentarios']."', '".$_POST['referencia']."')");
 
 //INICIO ENVÍO DE MENSAJE
-$asunto       = $_POST["nombre"] . ', Solicitud de DEMO';
+$asunto       =  'Solicitud de DEMO para '.$_POST['institucion'];
 $tituloMsj    = "¡Solicitud de DEMO de la Plataforma SINTIA!";
 $bgTitulo     = "#6017dc";
 $contenidoMsj = '
     <p>
-        Hola <b>' . strtoupper($_POST["nombre"]) . '</b>, Bienvenido a la Plataforma SINTIA. Te contactaremos para el demo pronto.<br>
+        Hola <b>' . strtoupper($_POST["nombre"]) . '</b>, Gracias por mostrar interés en la Plataforma SINTIA.<br>
+        Mi nombre es Jhon, y te estaré contactando en un momento para organizar el demo.<br>
+        A contiuación dejo tus datos recibidos a travez del formulario:<br>
+    </p>
+
+    <p>
+        Nombre: ' . $_POST["nombre"]. '<br>
+        Apellido: ' . $_POST["apellido"]. '<br>
+        Email: ' . $_POST["email"]. '<br>
+        Teléfono: ' . $_POST["telefono"]. '<br>
+        Institución: ' . $_POST["institucion"]. '<br>
+        Cargo: ' . $_POST["cargo"]. '<br>
+        Referencia: '. $_POST["referencia"]. '<br>
+        Comentarios: ' . $_POST["comentarios"]. '<br>
     </p>
 ';
 
